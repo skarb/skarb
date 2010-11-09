83dbcbcfd3ed66399cdbd1467c16ad819eae3693
Jan Stępień <jstepien@users.sourceforge.net>
2010-11-09 21:29:30 +0100

    Skrypt reaguje poprawnie na brak argumentów i -h

--- src/compiler/rubyc.rb
+++ src/compiler/rubyc.rb
@@ -1,2 +1,14 @@
 #!/usr/bin/env ruby
-puts 'See --help.'
+require 'optparse'
+
+ARGV.push '-h' if ARGV.empty?
+
+OptionParser.new do |opts|
+  opts.banner = "Usage: rubyc [options] input"
+  opts.separator "Options:"
+
+  opts.on("-h", "Show this message") do
+    puts opts.help
+    exit 0
+  end
+end.parse!
