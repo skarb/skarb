5672a53c1a3bbc7b2a8e90adf0ccb6f74aa5830d
Jan Stępień <jstepien@users.sourceforge.net>
2010-11-10 17:22:28 +0100

    Refactoring w compiler.rb

--- src/compiler/compiler.rb
+++ src/compiler/compiler.rb
@@ -1,4 +1,5 @@
 require 'tempfile'
+require 'fileutils'
 
 # Files created by Tempfile are placed in the /tmp directory. As a result all
 # files produced during the compilation process (excl. the output binary) are
@@ -9,25 +10,41 @@ class Compiler
       file.write code
       file.close
       spawn_cc file.path
-      spawn_linker file.path + '.o'
+      spawn_linker object_file file.path
+      clean_up file.path
     end
   end
 
   private
 
-  def spawn_cc(filename)
-    if (child = fork).nil?
-      exec "gcc -c -o #{filename}.o #{filename}"
-    end
-    Process.wait child
-    raise "gcc failed!" if $?.exitstatus != 0
+  # Returns an object file name for a given source file name.
+  def object_file(source_file)
+    source_file.sub /\.c$/, '.o'
   end
 
-  def spawn_linker(obj_file)
+  def spawn_cc(filename)
     if (child = fork).nil?
-      exec "gcc -o a.out #{obj_file}"
+      exec "gcc -c -o #{object_file filename} #{filename}"
     end
     Process.wait child
-    raise "gcc failed!" if $?.exitstatus != 0
+    raise_if_child_failed 'cc failed!'
+  end
+
+  def spawn_linker(filename)
+    if (child = fork).nil?
+      exec "gcc -o a.out #{filename}"
+    end
+    Process.wait child
+    raise_if_child_failed 'linker failed!'
+  end
+
+  # Removes artifacts after a successful compilation.
+  def clean_up(filename)
+    FileUtils.rm_f object_file filename
+  end
+
+  # Raises a given error if the child exited with a non-zero status.
+  def raise_if_child_failed(msg)
+    raise msg if $?.exitstatus != 0
   end
 end
