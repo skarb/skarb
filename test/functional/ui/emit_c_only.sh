c07d5960f8ebfd146de6b3f3d6f44d9d375af59c
Jan Stępień <jstepien@users.sourceforge.net>
2010-11-11 17:29:38 +0100

    Testy pod kątem parametru -C kompilatora

---
+++ test/functional/ui/emit_c_only.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+echo puts 0 | rubyc -C - || echo fail
+test -f out.c -a ! -f a.out && (file -i out.c | grep text/plain > /dev/null) || echo fail
+rm -f out.c a.out
--- test/functional/ui/help.sh
+++ test/functional/ui/help.sh
@@ -2,5 +2,6 @@
 #Usage: rubyc [options] input
 #Options:
 #    -o FILE                          Place the output into FILE
+#    -C                               Output the C code and exit
 #    -h                               Show this message
 rubyc -h
