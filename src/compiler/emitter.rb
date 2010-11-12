b6ce0dd1531a554c5f3fe58142ee2c01af7a5d19
Julian Zubek <julek@myhost.localdomain>
2010-11-12 15:33:10 +0100

    Zmiany w definicji C AST. Emiter przechodzi istniejące testy

--- src/compiler/emitter.rb
+++ src/compiler/emitter.rb
@@ -20,157 +20,145 @@ end
 
 # Generates code from given C abstract syntax tree. It does
 # not perform any validation.
-#
-# == Modifiers
-# Modifiers encapsulate variables definitions, they can be nested in
-# each other.
-# - :unsigned
-# - :signed
-# - :const
-# - :volatile
-# - :static
-# - :auto
-# - :extern
-# - :register
-#
-# == Flow control
-# - :if -- three child nodes: condition (:arg_expr), block (:block), "else" block (:block)
-# - :for -- four child nodes: init (:arg_expr), condition (:arg_expr),
-#   assignment (:arg_expr), code (:block)
-# - :while -- two child nodes: condition (:arg_expr), code (:block)
-# - :do -- two child nodes: condition (:arg_expr), code (:block)
-# - :switch -- two child nodes: expression (:arg_expr), code (:block)
-# - :case
-# - :goto
-# - :break
-# - :continue
-# - :return
-# - :default
-#
-# == Operators
-# - :binary_oper -- arithmetic, bitwise or logical operator
-# - :short_if -- "?:" operator
-# - :l_unary_oper -- left incrementation, decrementation and unary minus operator
-# - :r_unary_oper -- right incrementation and decrementation operator
-#
-# == Types
-# - :void
-# - :char
-# - :short
-# - :int
-# - :long
-# - :float
-# - :double
-# - :ctype -- custom type declared with typedef
-#
-# - :typedef
-# - :enum
-# - :union
-# - :struct -- structure definition
-# - :prototype -- function prototype
-#
-# - :var
-# - :svar_fld -- reference to field of a struct
-# - :svar_fld_ptr -- reference to field of a struct via pointer
-#
-# - :asgn -- assignment
-# - :call -- function call
-# - :defn -- function definition
-# - :abstract_args -- abstract parameters list
-# - :actual_args -- actual parameters list
-#
-# - :scope -- lexical scope of a function
-# - :block -- block of code consisting of multiple lines terminated by ';'
-
 class Emitter
 
-  def emit_struct(sexp)
-    @out << "typedef struct\n"
-    @out << "{\n"
-    sexp.rest(2).each { |elem| emit_generic_elem(elem) }
-    @out << "} " << sexp[1] << ";\n"
+  MinimalCode = "int main(){return 0;}"
+  
+  def emit(sexp)
+    return MinimalCode if sexp == nil
+    @out = StringIO.new
+    emit_generic_elem(sexp)
+    return @out.string
   end
 
-  def emit_prototype(sexp)
-    @out << sexp[1] << " " << sexp[2] << " ("
-    sexp[3].middle.each { |type| @out << type << ", " }
-    @out << sexp[3].last << ");\n"
-  end
-
-  def emit_if(sexp)
-    @out << "if ("
-    emit_arg_expr(sexp[1])
-    @out << ") "
-    if sexp[2]==:block
-      @out << "\n{\n"
-      emit_generic_elem(sexp[2])
-      @out << "}\n"
+  # Universal function for emitting any argument expression
+  # with correct parenthesis
+  def emit_arg_expr(elem)
+    case elem[0]
+    when :str, :lit, :var
+      emit_generic_elem(elem)
     else
-      emit_generic_elem(sexp[2])
-      @out << ";\n"
+      @out << "("
+      emit_generic_elem(elem)
+      @out << ")"
     end
-    unless sexp[3]==nil
-      @out << "else "
-      if sexp[3]==:block
-        @out << "\n{\n"
-        emit_generic_elem(sexp[3])
-        @out << "}\n"
+  end
+
+  # Emits symbol or executes method "emit_..." according sexp[0] symbol
+  def emit_generic_elem(sexp)
+    if sexp.class==Symbol
+      @out << sexp
+    elsif sexp.class==Sexp
+      begin
+        emit_method=method("emit_"+sexp[0].to_s)
+        emit_method.call(sexp)
+      rescue NameError
+        @out << sexp[0]
+        sexp.rest.each do |elem|
+          @out << " "
+          emit_generic_elem(elem)
+        end
+      end 
+    end  
+  end
+
+  
+  # Macro directives
+  # - :include
+  # - :define
+
+  def emit_include(sexp)
+    @out << "#include " << sexp[1]
+  end
+
+  def emit_define(sexp)
+    @out << "#define " << sexp[1]
+  end
+
+
+  # Blocks
+  # - :subblock -- multiple lines ended by ";"
+  # - :block -- "{" + subblock + "}"
+
+  def emit_block(sexp)
+    @out << "{\n"
+    emit_subblock(sexp)
+    @out << "}\n"
+  end
+
+  def emit_subblock(sexp)
+    sexp.rest.each do |elem|
+      case elem[0]
+      when :define, :include, :defn
+        emit_generic_elem(elem)
       else
-        emit_generic_elem(sexp[3])
+        emit_generic_elem(elem)
         @out << ";\n"
       end
     end
   end
 
-  def emit_or(sexp)
+
+  # Literals and vars
+  # - :lit -- literal
+  # - :str -- string literal
+  # - :var -- reference to variable
+  # - :decl -- variable declaration, two child nodes: type and name
+ 
+  def emit_str(sexp)
+    @out << '"' << sexp[1] << '"'
+  end
+
+  def emit_lit(sexp)
+    @out << sexp[1]
+  end
+ 
+  def emit_var(sexp)
+    @out << sexp[1]
+  end
+
+  def emit_decl(sexp)
+    @out << sexp[1] << " " << sexp[2]
+  end
+
+
+  # Assignments
+  # - :asgn -- normal assignment
+  # - :aasgn -- "*=", "-=", "/=", "+="
+  
+  def emit_asgn(sexp)
     emit_arg_expr(sexp[1])
-    @out << " || "
+    @out << " = "
     emit_arg_expr(sexp[2])
   end
 
-  def emit_and(sexp)
-    emit_arg_expr(sexp[1])
-    @out << " && "
+  def emit_aasgn(sexp)
     emit_arg_expr(sexp[2])
-  end
-
-  def emit_not(sexp)
-    @out << "!("
-    emit_arg_expr(sexp[1])
-    @out << ")"
-  end
-
-  def emit_short_if(sexp)
-    emit_arg_expr(sexp[1])
-    @out << " ? "
-    emit_arg_expr(sexp[2])
-    @out << " : "
+    @out << " " << sexp[1] << " "
     emit_arg_expr(sexp[3])
   end
 
-  def emit_abstract_args(sexp)
-    sexp.middle.each { |x| @out << x.first << " " << x.last << ", " }
-    if sexp.last!=nil
-      @out << sexp.last.first << " " << sexp.last.last
-    end
-  end
 
-  def emit_while(sexp)
-    if sexp[3]
-      @out << "while ("
-      emit_arg_expr(sexp[1])
-      @out << ")\n"
-      @out << "{\n"
-      emit_generic_elem(sexp[2])
-      @out << "}\n"
-    else
-      @out << "do\n"
-      @out << "{\n"
-      emit_generic_elem(sexp[2])
-      @out << "}\n"
-      @out << "while ("
-      emit_arg_expr(sexp[1])
-      @out << ");\n"
+  # Functions definitions and calls
+  # - :prototype -- function prototype
+  # - :defn -- function definition
+  # - :call -- function call
+  # - :abstract_args -- abstract parameters list
+  # - :actual_args -- actual parameters list
+
+  def emit_prototype(sexp)
+    @out << sexp[1] << " " << sexp[2] << " ("
+    emit_abstract_args(sexp[3])
+    @out << ")"
+  end
+ 
+  def emit_abstract_args(sexp)
+    sexp.middle.each do |x|
+      emit_generic_elem(x)
+      @out << ", "
+    end
+    if sexp.last!=nil
+      emit_generic_elem(sexp.last)
     end
   end
 
@@ -178,60 +166,7 @@ class Emitter
     @out << sexp[1] << " " << sexp[2] << "("
     emit_abstract_args(sexp[3])
     @out << ")\n"
-    emit_scope(sexp[4])
-  end
-
-  def emit_scope(sexp)
-    @out << "{\n"
-    sexp[1, sexp.length-1].each { |x| emit_block(x) }
-    @out << "}\n"
-  end
-
-  def emit_return(sexp)
-    @out << "return "
-    emit_arg_expr(sexp[1])
-    @out << ";\n"
-  end
-
-  # Universal function for emitting any argument expression
-  # with correct parenthesis
-  def emit_arg_expr(elem)
-    case elem[0]
-    when :call
-      @out << "("
-      emit_call(elem)
-      @out << ")"
-    when :asgn
-      @out << "("
-      emit_asgn(elem)
-      @out << ")"
-    when :binary_oper
-      @out << "("
-      emit_binary_oper(elem)
-      @out << ")"
-    when :if
-      @out << "("
-      emit_short_if(elem)
-      @out << ")"
-    when :or
-      @out << "("
-      emit_or(elem)
-      @out << ")"
-    when :and
-      @out << "("
-      emit_and(elem)
-      @out << ")"
-    when :not
-      emit_not(elem)
-    when :str
-      @out << '"' << elem[1] << '"'
-    when :lit
-      @out << elem[1]
-    when :lvar
-      @out << elem[1]
-    when :ivar
-      @out << elem[1] << "->" << elem[2]
-    end
+    emit_generic_elem(sexp[4])
   end
 
   def emit_actual_args(sexp)
@@ -250,36 +185,101 @@ class Emitter
     @out << ")"
   end
 
-  def emit_define(sexp)
-    @out << "#define " << sexp[1] << " "
-    # TODO
-    @out << sexp[2] << "\n"
+
+  # Flow control
+  # - :if -- three child nodes: condition (:arg_expr), block (:block), "else" block (:block)
+  # - :for -- four child nodes: init (:arg_expr), condition (:arg_expr),
+  #   assignment (:arg_expr), code (:block)
+  # - :while -- two child nodes: condition (:arg_expr), code (:block)
+  # - :do -- two child nodes: condition (:arg_expr), code (:block)
+  # - :switch -- two child nodes: expression (:arg_expr), code (:block)
+  # - :case
+  # - :goto
+  # - :label -- goto label
+  # - :break
+  # - :continue
+  # - :return
+  # - :default
+
+  def emit_if(sexp)
+    @out << "if ("
+    emit_arg_expr(sexp[1])
+    @out << ")\n"
+    emit_generic_elem(sexp[2])
+    unless sexp[3]==nil
+      @out << "else\n"
+      emit_generic_elem(sexp[3])
+    end
   end
 
-  def emit_int(sexp)
-    @out << "int " << sexp[1]
+  def emit_while(sexp)
+    @out << "while ("
+    emit_arg_expr(sexp[1])
+    @out << ")\n"
+    emit_generic_elem(sexp[2])
+    @out << "\n"
   end
 
-  def emit_include(sexp)
-    @out << "#include " << sexp[1] << "\n"
+  def emit_do(sexp)
+    @out << "do\n"
+    emit_generic_elem(sexp[2])
+    @out << "\n"
+    @out << "while ("
+    emit_arg_expr(sexp[1])
+    @out << ");\n"
   end
 
-  def emit_asgn(sexp)
-    @out << sexp[1] << " = "
+  def emit_for(sexp)
+    @out << "for ("
+    emit_generic_elem(sexp[1])
+    @out << "; "
+    emit_generic_elem(sexp[2])
+    @out << "; "
+    emit_generic_elem(sexp[3])
+    @out << ")\n"
+    emit_generic_elem(sexp[4])
+  end
+
+  def emit_switch(sexp)
+    @out << "switch ("
+    emit_generic_elem(sexp[1])
+    @out << ")\n"
+    emit_generic_elem(sexp[2])
+  end
+
+  def emit_default(sexp)
+    @out << "default: "
+  end
+
+  def emit_case(sexp)
+    @out << "case "
+    emit_generic_elem(sexp[1])
+    @out << ": "
+  end
+
+  def emit_label(sexp)
+    emit_generic_elem(sexp[1])
+    @out << ": "
+  end 
+
+
+  # Operators
+  # - :binary_oper -- arithmetic, bitwise, logical, "." or "->" operator
+  # - :short_if -- "?:" operator
+  # - :l_unary_oper -- left "++", "--", "-", "!", "*", "&" operator
+  # - :r_unary_oper -- right "++", "--" operator
+
+  def emit_short_if(sexp)
+    emit_arg_expr(sexp[1])
+    @out << " ? "
     emit_arg_expr(sexp[2])
-  end
-
-  def emit_struct_field(sexp)
-    @out << sexp[1] << "." << sexp[2]
-  end
-
-  def emit_ptr_struct_field(sexp)
-    @out << sexp[1] << "->" << sexp[2]
+    @out << " : "
+    emit_arg_expr(sexp[3])
   end
 
   def emit_binary_oper(sexp)
-    emit_arg_expr(sexp[1])
-    @out << " " << sexp[2] << " "
+    emit_arg_expr(sexp[2])
+    @out << " " << sexp[1] << " "
     emit_arg_expr(sexp[3])
   end
 
@@ -289,42 +289,27 @@ class Emitter
   end
 
   def emit_r_unary_oper(sexp)
-    emit_arg_expr(sexp[1])
-    @out << sexp[2]
+    emit_arg_expr(sexp[2])
+    @out << sexp[1]
   end
 
-  # Executes method "emit_..." according to sexp[0] symbol
-  def emit_generic_elem(sexp)
-    begin
-      emit_method=method("emit_"+sexp[0].to_s)
-      emit_method.call(sexp)
-    rescue NameError
-      raise "Invalid tree node: "+sexp[0].to_s
-    end
-  end
 
-  def emit_block(sexp)
-    sexp.rest.each do |elem|
-      next if elem==nil
-      case elem[0]
-      when :call
-        emit_call(elem)
-        @out << ";\n"
-      when :asgn
-        emit_asgn(elem)
-        @out << ";\n"
-      else
-        emit_generic_elem(elem)
-      end
-    end
-  end
+  # == Composite types
+  # - :typedef
+  # - :enum
+  # - :union
+  # - :struct
 
-  MinimalCode = "int main(){return 0;}"
+  # == Modifiers
+  # Modifiers encapsulate variables definitions, they can be nested in
+  # each other.
+  # - :unsigned
+  # - :signed
+  # - :const
+  # - :volatile
+  # - :static
+  # - :auto
+  # - :extern
+  # - :register
 
-  def emit(sexp)
-    return MinimalCode if sexp == nil
-    @out = StringIO.new
-    emit_generic_elem(sexp)
-    return @out.string
-  end
 end
--- test/unit/emitter_spec.rb
+++ test/unit/emitter_spec.rb
@@ -6,8 +6,8 @@ describe Emitter do
   before do
     @emitter = Emitter.new
     @hellow_body = Sexp.new(:block)
-    @hellow_main = Sexp.new(:defn, :int, :main, Sexp.new(:abstract_args, Sexp.new(:int, :argc),
-	Sexp.new(:'char**', :args)), Sexp.new(:scope, @hellow_body))
+    @hellow_main = Sexp.new(:defn, :int, :main, Sexp.new(:abstract_args, Sexp.new(:decl, :int, :argc),
+	Sexp.new(:decl, :'char**', :args)), @hellow_body)
     @hellow_body.push(Sexp.new(:call, :printf, Sexp.new(:actual_args, Sexp.new(:str, "Hello world!"))))
     @hellow_body.push(Sexp.new(:return, Sexp.new(:lit, 0)))
   end
@@ -17,97 +17,73 @@ describe Emitter do
       "int main(int argc, char** args)\n{\nprintf(\"Hello world!\");\nreturn 0;\n}\n"
   end
 
-  it 'should emit int variable declaration' do
-    @emitter.emit(Sexp.new(:int, :a)).should ==
-      "int a"
-  end
-
-=begin
-  it 'should emit char variable declaration' do
-    @emitter.emit(Sexp.new(:char, :a)).should ==
-      "char a"
-  end
-
-  it 'should emit short variable declaration' do
-    @emitter.emit(Sexp.new(:short, :a)).should ==
-      "short a"
-  end
-
-  it 'should emit long variable declaration' do
-    @emitter.emit(Sexp.new(:long, :a)).should ==
-      "long a"
-  end
-
-  it 'should emit float variable declaration' do
-    @emitter.emit(Sexp.new(:float, :a)).should ==
-      "float a"
-  end
-
-  it 'should emit double variable declaration' do
-    @emitter.emit(Sexp.new(:double, :a)).should ==
-      "double a"
-  end
-
-  it 'should emit ctype variable declaration' do
-    @emitter.emit(Sexp.new(:ctype, :my_type, :a)).should ==
+  it 'should emit variable declaration' do
+    @emitter.emit(Sexp.new(:decl, :my_type, :a)).should ==
       "my_type a"
   end
 
   it 'should emit unsigned modifier' do
-    @emitter.emit(Sexp.new(:unsigned, Sexp.new(:int, a))).should ==
+    @emitter.emit(Sexp.new(:unsigned, Sexp.new(:decl, :int, :a))).should ==
      "unsigned int a"
   end 
 
   it 'should emit signed modifier' do
-    @emitter.emit(Sexp.new(:signed, Sexp.new(:int, a))).should ==
+    @emitter.emit(Sexp.new(:signed, Sexp.new(:decl, :int, :a))).should ==
      "signed int a"
   end 
 
   it 'should emit const modifier' do
-    @emitter.emit(Sexp.new(:const, Sexp.new(:int, a))).should ==
+    @emitter.emit(Sexp.new(:const, Sexp.new(:decl, :int, :a))).should ==
      "const int a"
   end 
 
   it 'should emit volatile modifier' do
-    @emitter.emit(Sexp.new(:volatile, Sexp.new(:int, a))).should ==
+    @emitter.emit(Sexp.new(:volatile, Sexp.new(:decl, :int, :a))).should ==
      "volatile int a"
   end 
 
   it 'should emit static modifier' do
-    @emitter.emit(Sexp.new(:static, Sexp.new(:int, a))).should ==
+    @emitter.emit(Sexp.new(:static, Sexp.new(:decl, :int, :a))).should ==
      "static int a"
   end 
 
   it 'should emit auto modifier' do
-    @emitter.emit(Sexp.new(:auto, Sexp.new(:int, a))).should ==
+    @emitter.emit(Sexp.new(:auto, Sexp.new(:decl, :int, :a))).should ==
      "auto int a"
   end 
 
   it 'should emit extern modifier' do
-    @emitter.emit(Sexp.new(:extern, Sexp.new(:int, a))).should ==
+    @emitter.emit(Sexp.new(:extern, Sexp.new(:decl, :int, :a))).should ==
      "extern int a"
   end 
 
   it 'should emit register modifier' do
-    @emitter.emit(Sexp.new(:register, Sexp.new(:int, a))).should ==
+    @emitter.emit(Sexp.new(:register, Sexp.new(:decl, :int, :a))).should ==
      "register int a"
   end 
 
   it 'should emit if clause' do
     @emitter.emit(Sexp.new(:if, Sexp.new(:lit, 1),
-                           Sexp.new(:return, Sexp.new(:lit, 1)),
-                           Sexp.new(:return, Sexp.new(:lit, 0)))).should ==
-     "if(1)\n{\nreturn 1;\n} else\n{\nreturn 0;}"
+                           Sexp.new(:subblock,
+                           Sexp.new(:return, Sexp.new(:lit, 1))),
+                           Sexp.new(:subblock,
+                           Sexp.new(:return, Sexp.new(:lit, 0))))).should ==
+     "if (1)\nreturn 1;\nelse\nreturn 0;\n"
   end 
 
+  it 'should emit assignment' do
+    @emitter.emit(Sexp.new(:asgn, Sexp.new(:var, :a), Sexp.new(:lit, 2))).should ==
+      "a = 2"
+  end
+
   it 'should emit for clause, assignment, right unary operator, lvar' do
-    @emitter.emit(Sexp.new(:for, Sexp.new(:asgn, Sexp.new(:lvar, :a),
+    @emitter.emit(Sexp.new(:for, Sexp.new(:asgn, Sexp.new(:var, :a),
                                           Sexp.new(:lit, 2)),
                            Sexp.new(:lit, 1), Sexp.new(:r_unary_oper, :'++',
-                                                       Sexp.new(:lvar, :a)),
-                           Sexp.new(:block, Sexp.new(:return,
+                                                       Sexp.new(:var, :a)),
+                           Sexp.new(:subblock, Sexp.new(:return,
                                                      Sexp.new(:lit, 1))))).should ==
-     "for(a = 2; 1; a++)\n{\nreturn 1;\n}"
+     "for (a = 2; 1; a++)\nreturn 1;\n"
   end 
 
   it 'should emit binary operator' do
@@ -117,19 +93,19 @@ describe Emitter do
   end
 
   it 'should emit left unary operator' do
-    @emitter.emit(Sexp.new(:l_unary_oper, :>, :-, Sexp.new(:lit, 1))).should ==
+    @emitter.emit(Sexp.new(:l_unary_oper, :-, Sexp.new(:lit, 1))).should ==
      "-1"
   end
 
   it 'should emit switch' do
     @emitter.emit(Sexp.new(:switch, Sexp.new(:lit, 1),
                           Sexp.new(:block))).should ==
-      "switch(1)\n{\n}"
+      "switch (1)\n{\n}\n"
   end
 
   it 'should emit default' do
     @emitter.emit(Sexp.new(:default)).should ==
-      "default:"
+      "default: "
   end
 
   it 'should emit break' do
@@ -149,7 +125,7 @@ describe Emitter do
 
   it 'should emit case' do
     @emitter.emit(Sexp.new(:case, Sexp.new(:lit, 1))).should ==
-      "case 1:"
+      "case 1: "
   end
 
   it 'should emit short if' do
@@ -165,34 +141,28 @@ describe Emitter do
 
   it 'should emit typedef with struct' do
     @emitter.emit(Sexp.new(:typedef,
-                           Sexp.new(:struct, :_my_struct,
+                           Sexp.new(:struct, nil,
                                      Sexp.new(:block,
-                                              Sexp.new(:int, :a))),
+                                              Sexp.new(:decl, :int, :a))),
                            :my_struct)).should ==
-      "typedef struct _my_struct\n{\nint a;\n}\nmy_struct"
+      "typedef struct  {\nint a;\n}\n my_struct"
   end
 
   it 'should emit union' do
     @emitter.emit(Sexp.new(:union, :my_union,
-                           Sexp.new(:block, Sexp.new(:int, :a)))).should ==
-      "union my_union\n{\nint a;\n}"
+                           Sexp.new(:block, Sexp.new(:decl, :int, :a)))).should ==
+      "union my_union {\nint a;\n}\n"
   end
 
   it 'should emit prototype' do
     @emitter.emit(Sexp.new(:prototype, :int, :foo,
-                           Sexp.new(:abstract_args, Sexp.new(:int, :a)))).should ==
-      "int foo(int a)"
+                           Sexp.new(:abstract_args, Sexp.new(:decl, :int, :a)))).should ==
+      "int foo (int a)"
   end
 
-  it 'should emit svar_fld' do
-    @emitter.emit(Sexp.new(:svar_fld, :my_struct, :my_fld)).should ==
-      "my_struct.my_fld"
+  it 'should emit goto label' do
+    @emitter.emit(Sexp.new(:label, :abc)).should ==
+      "abc: "
   end
-
-  it 'should emit svar_fld_ptr' do
-    @emitter.emit(Sexp.new(:svar_fld, :my_struct, :my_fld)).should ==
-      "my_struct->my_fld"
-  end
-=end
 
 end
