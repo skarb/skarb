69f41bc93a929c3dce1542d2aa98cfb61a783d2a
Jan Stępień <jstepien@users.sourceforge.net>
2010-11-12 02:49:16 +0100

    Garstka komentarzy w parser.rb i translator.rb

--- src/compiler/parser.rb
+++ src/compiler/parser.rb
@@ -1,6 +1,9 @@
 require 'ruby_parser'
 
+# Responsible for parsing Ruby source code and creating ASTs. Behind the scenes
+# it uses RubyParser.
 class Parser
+  # Reads contents of a given opened file and returns an AST for it.
   def parse(file)
     begin
       RubyParser.new.parse(file.read)
