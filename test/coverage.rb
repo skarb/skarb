42774bcdb30cf02b94a29e92665c7be24ec17d2e
Jan Stępień <jstepien@users.sourceforge.net>
2010-11-15 23:14:27 +0100

    Testy funkcjonalne są wliczane w procent pokrycia

--- test/coverage.rb
+++ test/coverage.rb
@@ -16,4 +16,12 @@ SimpleCov.start do
   root ENV['top_srcdir']
 end
 
+# Running unit tests
 Dir.glob(ENV['srcdir'] + '/unit/*_spec.rb').each { |file| require file }
+
+# Running functional tests
+ENV['TESTS'] = Dir.glob(ENV['srcdir'] + '/functional/*.rb') \
+  .delete_if { |f| f =~ /runner\.rb/ } .map { |f| File.basename f } .join ' '
+functional_tests_runner = ENV['srcdir'] + '/functional/runner.rb'
+ENV['srcdir'] += '/functional'
+load functional_tests_runner
