efd47bdef8ad26688a3ee2aca4388130dad79b26
Jan Stępień <jstepien@users.sourceforge.net>
2010-11-15 09:32:19 +0100

    Test uwzględnienia CFLAGS, zmiany w teście CC

--- test/functional/ui/use_cc_by_default.sh
+++ test/functional/ui/use_cc_by_default.sh
@@ -2,5 +2,5 @@
 #acts_as_cc
 #acts_as_cc
 ruby_fullpath=`which $RUBY`
-export PATH="$srcdir:`dirname $ruby_fullpath`"
+export PATH="$srcdir:`dirname $ruby_fullpath`:$PATH"
 echo 0 | rubyc - || echo fail
