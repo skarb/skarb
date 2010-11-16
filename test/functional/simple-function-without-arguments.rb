2aad551e332e99bb63c294699535a207faf54611
Jan Stępień <jstepien@users.sourceforge.net>
2010-11-16 22:57:48 +0100

    Test definicji funkcji wewnątrz funkcji

--- test/functional/simple-function-without-arguments.rb
+++ test/functional/simple-function-without-arguments.rb
@@ -3,6 +3,9 @@
 #3
 #5
 #3
+#7
+#9
+#6
 def fun
   puts 3
 end
@@ -14,6 +17,16 @@ def fun2
   puts 5
 end
 
+def fun3
+  puts 7
+  def fun4
+    puts 6
+  end
+  puts 9
+  fun4
+end
+
 fun
 fun2
 fun
+fun3
