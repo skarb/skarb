260eb3d9d12ccacc6e41f91db31e029adb0a4b98
Jan Stępień <jstepien@users.sourceforge.net>
2010-11-16 14:31:10 +0100

    Ruby weryfikuje poprawność testów funkcjonalnych
    TODO: extract_expected_output powinno być w osobnym pliku

---
+++ scripts/verify_functional_tests.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+require 'rspec'
+
+ruby = ARGV.shift || 'ruby'
+
+def extract_expected_output(file)
+  lines = File.open(file).readlines
+  lines.shift if lines.first =~ /^#!/
+  lines = lines.take_while{ |line| line =~ /^#/ }
+  lines.map { |line| line.sub(/^#/, '') } .join
+end
+
+tests = Dir.glob('test/functional/*.rb') .delete_if { |f| f =~ /runner\.rb/ }
+
+describe 'Functional test' do
+  tests.each do |file|
+    it "#{File.basename file} should be valid in #{ruby}" do
+      IO.popen(ruby + ' ' + file).read.should == extract_expected_output(file)
+    end
+  end
+end
