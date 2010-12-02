64cec5eccfa2d8692922ada50993c5f840f6802a
Jan Stępień <jstepien@users.sourceforge.net>
2010-12-02 12:42:02 +0100

    Naprawy Translator::Functions
    Funkcje z argumentami są już poprawnie wołane więcej niż raz.

--- src/compiler/translator/functions.rb
+++ src/compiler/translator/functions.rb
@@ -3,78 +3,43 @@ class Translator
   # to defining and calling functions and methods.
   module Functions
     # Translates a call to the Kernel#puts method or a simple function defined
-    # previously. All other calls cause an error. TODO: split it into multiple
-    # methods.
+    # previously. All other calls cause an error.
     def translate_call(sexp)
       # Only simple functions without arguments are supported.
       raise UnsupportedNodeError unless sexp[1].nil? or sexp[3].count > 1
-      var = next_var_name
-      args_evaluation = s(:stmts)
-      call_arguments = s(:args)
       if sexp[2] == :puts
-        call = faked_puts sexp
-      elsif defn = @functions_definitions[sexp[2]]
-        # If the function has been already defined translate it's body and save
-        # the output in @functions_implementations.
-        unless @functions_implementations.has_key? defn[1]
-          @symbol_table.cfunction = defn[1]
-          defn_args = s(:args)
-          call_args_eval_stmts = []
-          call_arg_number = 1
-          defn[2].drop(1).each do |arg|
-            @symbol_table.add_lvar arg
-            # FIXME: set the actual type
-            @symbol_table.set_lvar_types arg, [Fixnum]
-            defn_args << s(:decl, 'Fixnum*', arg)
-            args_evaluation << translate_generic_sexp(sexp[3][call_arg_number])
-            call_arguments << args_evaluation.last.value_symbol
-            call_arg_number += 1
-          end
-          args_evaluation = filtered_stmts(*args_evaluation.rest)
-          body = translate_generic_sexp(defn[3][1])
-          body_block = filtered_block(body, s(:return, body.value_symbol))
-          # FIXME: set the actual return type
-          @functions_implementations[defn[1]] = s(:defn, 'Fixnum*', defn[1],
-                                                  defn_args, body_block)
-          @symbol_table.cfunction = :_main
-        end
-        call = s(:call, defn[1], call_arguments)
+        call_faked_puts sexp
+      elsif function_is_defined? sexp[2]
+        call_defined_function sexp
       else
         raise "Unknown function: #{sexp[1]}"
       end
-      filtered_stmts(
-        args_evaluation,
-        s(:decl, 'Fixnum*', var),
-        s(:asgn, s(:var, var), call)
-      ).with_value_symbol s(:var, var)
     end
 
     # Returns a sexp which acts as if the Kernel#puts method was called.
-    def faked_puts(sexp)
+    def call_faked_puts(sexp)
       value = sexp[3][1]
       case value[0]
       when :lit
-        return s(:call, :Fixnum_new,
-                 s(:args, s(:call, :printf,
-                            s(:args, s(:str, '%i\n'), s(:lit, value[1])))))
+        retval = s(:call, :printf,
+                   s(:args, s(:str, '%i\n'), s(:lit, value[1])))
       when :lvar
         raise 'Unknown local variable' if not @symbol_table.has_lvar? value[1]
         type = @symbol_table.get_lvar_types(value[1]).first
         if type == Fixnum
-          return s(:call, :Fixnum_new,
-                   s(:args, s(:call, :printf,
-                              s(:args, s(:str, '%i\n'),
-                                s(:binary_oper, :'->',
-                                  s(:var, value[1]), s(:var, :val))))))
+          retval = s(:call, :printf,
+                     s(:args, s(:str, '%i\n'),
+                       s(:binary_oper, :'->',
+                         s(:var, value[1]), s(:var, :val))))
         elsif type == Float
-          return s(:call, :Fixnum_new,
-                   s(:args, s(:call, :printf,
-                              s(:args, s(:str, '%g\n'),
-                                s(:binary_oper, :'->',
-                                  s(:var, value[1]), s(:var, :val))))))
+          retval = s(:call, :printf,
+                     s(:args, s(:str, '%g\n'),
+                       s(:binary_oper, :'->',
+                         s(:var, value[1]), s(:var, :val))))
         end
       end
-      raise 'Only Fixnums can be printed'
+      raise 'Only Fixnums can be printed' if not retval
+      retval.with_value_symbol s(:lit, 0)
     end
 
     # Functions' definitions don't get translated immediately. We'll wait for the
@@ -83,6 +48,57 @@ class Translator
     def translate_defn(sexp)
       @functions_definitions[sexp[1]] = sexp
       s(:stmts)
+    end
+
+    private
+
+    # Returns true if a given function (not a method!) has been already defined.
+    def function_is_defined?(name)
+      @functions_definitions.include? name
+    end
+
+    # Returns a sexp calling a defined function. If the function hasn't been
+    # implemented yet implement_function is called.
+    def call_defined_function(sexp)
+      var = next_var_name
+      # Evaluate arguments. We need to know what their type is in order to call
+      # appropriate overloaded function.
+      args_evaluation = sexp[3].rest.map { |arg_sexp| translate_generic_sexp arg_sexp }
+      # Get the defn sexp in which the function has been defined.
+      defn = @functions_definitions[sexp[2]]
+      implement_function defn unless function_has_implementation? defn
+      call = s(:call, defn[1],
+               s(:args, *args_evaluation.map { |arg| arg.value_symbol } ))
+      filtered_stmts(
+        filtered_stmts(*args_evaluation),
+        s(:decl, 'Fixnum*', var),
+        s(:asgn, s(:var, var), call)
+      ).with_value_symbol s(:var, var)
+    end
+
+    # Returns true if a given function (not a method!) defined with a given defn
+    # sexp has been already added to @functions_implementations.
+    def function_has_implementation?(defn)
+      @functions_implementations.has_key? defn[1]
+    end
+
+    # Translates a given function and adds its implementation to
+    # @functions_implementations.
+    def implement_function(defn)
+      @symbol_table.cfunction = defn[1]
+      defn_args = s(:args)
+      defn[2].drop(1).each do |arg|
+        @symbol_table.add_lvar arg
+        # FIXME: set the actual type
+        @symbol_table.set_lvar_types arg, [Fixnum]
+        defn_args << s(:decl, 'Fixnum*', arg)
+      end
+      body = translate_generic_sexp(defn[3][1])
+      body_block = filtered_block(body, s(:return, body.value_symbol))
+      # FIXME: set the actual return type
+      @functions_implementations[defn[1]] = s(:defn, 'Fixnum*', defn[1],
+                                              defn_args, body_block)
+      @symbol_table.cfunction = :_main
     end
   end
 end
