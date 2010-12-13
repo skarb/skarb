4f588cfe265c4de1486a23acc9e88ea732619532
Jan Stępień <jstepien@users.sourceforge.net>
2010-12-13 15:56:24 +0100

    Odchudzono t/f/ui/errorous_source.rb

--- test/functional/ui/errorous_source.rb
+++ test/functional/ui/errorous_source.rb
@@ -1,19 +1,4 @@
-require 'rspec'
-
-compiler = ARGV.shift
-
-def extract_expected_output(file)
-  lines = File.open(file).readlines
-  lines.shift if lines.first =~ /^#!/
+lines = 4
+lines.shift if lines.first =~ /^#!/
 35W /*33$&^*\/&^/76945/  lines = lines.take_while{ |line| line =~ /^#/ }
-  lines.map { |line| line.sub(/^#/, '') } .join
-end
-
-describe 'UI' do
-  Dir.glob('*.sh').reject { |f| f == 'testlib.sh' } .each do |file|
-    it "should pass #{file}" do
-      output = IO.popen("sed -e 's,rubyc,#{compiler},g' #{file} | sh").read
-      output.should == extract_expected_output(file)
-    end
-  end
-end
+lines.map { |line| line.sub(/^#/, '') } .join
