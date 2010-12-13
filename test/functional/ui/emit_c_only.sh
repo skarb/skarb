27c73f0e87a883f1d9af8a7cd65b48ef91b5f28b
Jan Stępień <jstepien@users.sourceforge.net>
2010-12-13 14:36:58 +0100

    Złamano za długą linijkę w t/f/ui/emit_c_only.sh

--- test/functional/ui/emit_c_only.sh
+++ test/functional/ui/emit_c_only.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 echo 0 | rubyc -C - || echo fail
-test -f out.c -a ! -f a.out && (file -i out.c | grep text/x-c > /dev/null) || echo fail
+test -f out.c -a ! -f a.out && (file -i out.c | grep text/x-c > /dev/null) \
+    || echo fail
 rm -f out.c a.out
