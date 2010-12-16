e0e8186470a472f9e7a5f0d3678f4cffed59b790
Jan Stępień <jstepien@users.sourceforge.net>
2010-12-16 13:21:20 +0100

    Obsługa tablic definiowanych przy pomocy [ ]

--- src/compiler/translator/constants.rb
+++ src/compiler/translator/constants.rb
@@ -33,7 +33,20 @@ class Translator
 
     # Translates an array literal, such as [1, 2, "three"].
     def translate_array(sexp)
-      s().with_value(s(:call, :Array_new, s(:args)), Array)
+      if sexp.count == 1
+        s().with_value s(:call, :Array_new, s(:args)), :Array
+      else
+        args = sexp.rest.map { |child| translate_generic_sexp child }
+        var = next_var_name
+        filtered_stmts(
+          s(:decl, :'Object*', var),
+          s(:asgn, s(:var, var), s(:call, :Array_new, s(:args))),
+          filtered_block(
+            *args,
+            *args.map { |arg| s(:call, :Array_push,
+                                s(:args, s(:var, var), arg.value_sexp)) }
+        )).with_value s(:var, var), :Array
+      end
     end
   end
 end
