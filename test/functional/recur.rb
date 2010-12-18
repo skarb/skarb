c455813694dd3af79469342bb0a6613787e4ce72
Jan Stępień <jstepien@users.sourceforge.net>
2010-12-18 14:55:02 +0100

    Do testów rekurencji dodano funkcję Ackermanna

--- test/functional/recur.rb
+++ test/functional/recur.rb
@@ -1,4 +1,5 @@
 #720
+#253
 def fact(x)
   if x < 2
     1
@@ -8,3 +9,15 @@ def fact(x)
 end
 
 puts fact 6
+
+def ackermann(m, n)
+  if m.zero?
+    n + 1
+  elsif n.zero?
+    ackermann m - 1, 1
+  else
+    ackermann m - 1, ackermann(m, n - 1)
+  end
+end
+
+puts ackermann(3,5)
