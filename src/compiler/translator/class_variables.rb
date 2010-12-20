06a62cc19fe37188ca595d963e1630016b0cbe4b
Julian Zubek <julek@myhost.localdomain>
2010-12-20 14:36:35 +0100

    Dodano brakujący plik class_variables.rb

---
+++ src/compiler/translator/class_variables.rb
@@ -0,0 +1,46 @@
+class Translator
+  # A module consisting of functions which handle translation of nodes related
+  # to class variables.
+  #
+  # Types of class variables are always unknown.
+  module ClassVariables
+    # Translate assignment to a class variable. The variable is declared
+    # unless it already was. As a value of expression the variable is returned.
+    def translate_cvasgn(sexp)
+      str_name = sexp[1].to_s
+      sname = str_name[2, str_name.length-1].to_sym
+      unless @symbol_table.has_cvar? sexp[1]
+        @symbol_table.add_cvar sexp[1]
+      end
+      arg = translate_generic_sexp(sexp[2])
+      val_type = arg.value_type
+      cvar_class = @symbol_table.get_cvar_class sexp[1]
+      field_name =  s(:binary_oper, :'->',
+                              s(:cast, ('s'+cvar_class.to_s+'*').to_sym,
+                                s(:var,
+                                  :"classes_dictionary[
+                                    #{@symbol_table[cvar_class][:id]}].fields")),
+                              s(:var, sname))
+      filtered_stmts(arg, s(:asgn, field_name, arg.value_sexp))
+      .with_value_sexp field_name
+    end
+
+    alias :translate_cvdecl :translate_cvasgn
+
+    # Translate a referenced class variable to empty block with value of this
+    # variable.
+    def translate_cvar(sexp)
+      unless @symbol_table.has_cvar? sexp[1]
+        @symbol_table.add_cvar sexp[1]
+      end
+      str_name = sexp[1].to_s
+      sname = str_name[2, str_name.length-1].to_sym
+      cvar_class = @symbol_table.get_cvar_class sexp[1]
+      s(:stmts).with_value_sexp s(:binary_oper, :'->',
+          s(:cast, ('s'+cvar_class.to_s+'*').to_sym,
+            s(:var, :"classes_dictionary[
+              #{@symbol_table[cvar_class][:id]}].fields")),
+          s(:var, sname))
+    end
+  end
+end
