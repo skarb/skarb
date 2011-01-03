242d6c85ca7891deef3254171e5f8541bbda8e72
Julian Zubek <julek@myhost.localdomain>
2011-01-03 18:57:27 +0100

    Dodatkowy test dla instrukcji return

--- test/functional/return.rb
+++ test/functional/return.rb
@@ -1,5 +1,6 @@
 #3
 #foobarasdf
+#0
 def foo(x)
   return 2 if x < 5
   "foobar"
@@ -7,3 +8,14 @@ end
 
 puts (foo 1) + 1
 puts (foo 6) + "asdf"
+
+def r_b(i)
+  r_a(i-1)
+end
+
+def r_a(i)
+  return r_b(i) if i>0
+  i
+end
+
+puts r_a(5)
