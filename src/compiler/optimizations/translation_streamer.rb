c236f9e6ca61448aafb8d3f1542633c951a682eb
Julian Zubek <julek@myhost.local>
2012-05-11 23:31:53 +0200

    Translation streamer functional.

--- src/compiler/optimizations/translation_streamer.rb
+++ src/compiler/optimizations/translation_streamer.rb
@@ -2,15 +2,22 @@ require 'sexp_processor'
 require 'translator/event_manager'
 
 # This class receives all sexps translated by the translator, goes through the
-# translated code and fires event for each translated C code sexp. 
+# translated code and fires event for each translated C code sexp. It also can
+# tell at any given moment to which function C streamed code belongs (it is
+# non-trivial, because code is streamed in execution order, not structural order).
 class TranslationStreamer
 
    EventStruct = Struct.new(:sexp, :original_sexp)
 
+   attr_reader :cfunction
+
    def initialize(translator)
-      @translator = translator
+      @s_table = translator.symbol_table
       @event_manager = EventManager.new
-      @translator.subscribe(:generic_sexp_translated, self.method(:sexp_translated))
+      translator.subscribe(:generic_sexp_translated, self.method(:sexp_translated))
+      @s_table.subscribe(:function_opened, self.method(:function_opened))
+      @s_table.subscribe(:function_closed, self.method(:function_closed))      
+      @s_table.subscribe(:cfunction_changed, self.method(:cfunction_changed))      
    end
 
    def subscribe(event, method)
@@ -21,12 +28,52 @@ class TranslationStreamer
       traverse_sexp(event.translated_sexp, event.original_sexp)
    end
 
+   def function_opened(event)
+      @event_manager.fire_event(:function_opened, nil)
+   end
+
+   def function_closed(event)
+      @event_manager.fire_event(:function_closed, nil)
+   end
+
+   def cfunction_changed(event)
+      @cfunction = event.new_value
+      @event_manager.fire_event(:cfunction_changed, event)
+   end
+
+   # Traverses translated C sexp and fires appropriate event for each of its
+   # children. To ensure that there will be no duplicates, each visited sexp
+   # is marked.
    def traverse_sexp(sexp, original_sexp)
-      if sexp.is_a? Sexp
+      if sexp.is_a? Sexp and !sexp.marked?
+   
+         mark_sexp(sexp)
+
+         if sexp[0] == :block
+            @event_manager.fire_event(:block_opened, nil)
+         end
+
          sexp.each { |a| traverse_sexp(a, original_sexp) }
+
+         if sexp[0] == :block
+            @event_manager.fire_event(:block_closed, nil)
+         end
+
          @event_manager.fire_event("#{sexp[0]}_translated".to_sym,
                                    EventStruct.new(sexp, original_sexp)) 
       end 
+   end
+
+   # All sexp are initially not marked.
+   def Sexp.marked?
+      false
+   end
+
+   # This method marks sexp by redefining its "marked?" method.
+   def mark_sexp(sexp)
+      def sexp.marked?
+         true
+      end
    end
 
 end
--- test/unit/translation_streamer_spec.rb
+++ test/unit/translation_streamer_spec.rb
@@ -9,15 +9,47 @@ describe TranslationStreamer do
     @translator = Translator.new
     @streamer = TranslationStreamer.new(@translator)
     @streamer.subscribe(:lit_translated, self.method(:lit_translated))
+    @streamer.subscribe(:block_opened, self.method(:block_opened))
+    @streamer.subscribe(:block_closed, self.method(:block_closed))
+    @streamer.subscribe(:cfunction_changed, self.method(:cfunction_changed))
+    @lit_count = 0
+    @opened = 0
+    @closed = 0
+    @functions = []
   end
 
   def lit_translated(event)
     @lit_count += 1
+    p event.sexp
+  end
+
+  def block_opened(event)
+    @opened += 1
+  end
+
+  def block_closed(event)
+    @closed += 1
+  end
+
+  def cfunction_changed(event)
+    @functions << event.new_value
+    puts "#{event.old_value} -> #{event.new_value}"
   end
 
   it 'should fire event for every C sexp in translated code' do
-    @lit_count = 0
     @translator.translate(Parser.parse("1"))
     @lit_count.should > 0
   end
+
+  it 'should generate block events' do
+    @translator.translate(Parser.parse("if 1; a = 1; else; a = 2; end"))
+    @opened.should == 2
+    @closed.should == 2
+  end
+
+  it 'should keep track of current function context' do
+    @translator.translate(Parser.parse("a = 10; def foo; a = 20; end; a = 30; def bar; foo; end; a = 40"))
+    @functions.should == [:M_Object_foo, :M_Object_bar] 
+  end
+
 end
