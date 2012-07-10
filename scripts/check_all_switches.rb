0c7e81df52db8bc30fcb76731a9159a525e6f54b
Julian Zubek <julek@myhost.local>
2012-07-10 14:19:33 +0200

    Scripts for testing compiler with all switches.

---
+++ scripts/check_all_switches.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/ruby
+
+PROJECT_DIR = "/home/julek/projects/mgr/"
+
+LegalSwitches = ["--math_inline", "--stack_alloc"]
+
+Dir.chdir(PROJECT_DIR)
+
+0.upto(LegalSwitches.length) do |i|
+   LegalSwitches.combination(i) do |c|
+      `RUBYC_FLAGS="#{c.join(' ')}" make check > check_results_#{c.join('_')}`
+   end
+end
