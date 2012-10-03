c2e13b8e318a43901259c42f7dc9308371f589df
Julian Zubek <julek@myhost.local>
2012-10-03 23:34:19 +0200

    Added copyright and license notices.

--- src/compiler/emitter/literals.rb
+++ src/compiler/emitter/literals.rb
@@ -21,3 +21,22 @@ module Emitter::Literals
     sexp[1,2].join ' '
   end
 end
+
+##################################################################################
+# (C) 2010-2012 Jan Stępień, Julian Zubek
+# 
+# This file is a part of Skarb -- Ruby to C compiler.
+# 
+# Skarb is free software: you can redistribute it and/or modify it under the terms
+# of the GNU Lesser General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+# 
+# Skarb is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public License along
+# with Skarb. If not, see <http://www.gnu.org/licenses/>.
+# 
+##################################################################################
