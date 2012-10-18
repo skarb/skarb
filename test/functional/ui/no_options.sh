0200bafd1f2c52a37a8547fa7b73600fae379724
Jan Stępień <jstepien@users.sourceforge.net>
2012-10-18 19:50:12 +0200

    Fixed UI tests: added new command line options to the usage message

--- test/functional/ui/no_options.sh
+++ test/functional/ui/no_options.sh
@@ -4,5 +4,8 @@
 #    -o FILE                          Place the output into FILE
 #    -C                               Output the C code and exit
 #    -c                               Don't link, only compile
+#        --math_inline                Use C operators in math expressions
+#        --stack_alloc                Use stack allocation for local objects
+#        --object_reuse               Reuse memory of dead local objects
 #    -h                               Show this message
 skarb
